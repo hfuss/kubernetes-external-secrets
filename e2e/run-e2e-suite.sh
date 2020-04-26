851699cdf0f380b7c34f02eee72053d3dcc2630e
Hayden Fuss <wifu1234@gmail.com>
2020-04-26 14:13:03 -0400

    template syntax differs between v2 and v3

--- e2e/run-e2e-suite.sh
+++ e2e/run-e2e-suite.sh
@@ -70,23 +70,26 @@ trap cleanup EXIT
 
 kubectl apply -f ${DIR}/localstack.deployment.yaml
 
-HELM_TEMPLATE_EXTRA_ARGS=""
-E2E_EXTRA_ARGS=""
+HELM_TEMPLATE_ARGS="e2e '$(dirname "$DIR")/charts/kubernetes-external-secrets'"
+HELM_TEMPLATE_EXTRA_ARGS="--include-crds --set customResourceManagerDisabled=true"
+E2E_EXTRA_ARGS='--env="DISABLE_CUSTOM_RESOURCE_MANAGER=true"'
 if [[ "$HELM_VERSION" == "V3" ]]; then
-  if [[ "$DISABLE_CUSTOM_RESOURCE_MANAGER" == "true" ]]; then
-    HELM_TEMPLATE_EXTRA_ARGS="--include-crds --set customResourceManagerDisabled=true"
-    E2E_EXTRA_ARGS='--env="DISABLE_CUSTOM_RESOURCE_MANAGER=true"'
-  else
+  if [[ "$DISABLE_CUSTOM_RESOURCE_MANAGER" == "false" ]]; then
     HELM_TEMPLATE_EXTRA_ARGS="--skip-crds"
+    E2E_EXTRA_ARGS=''
   fi
 else
+  HELM_TEMPLATE_ARGS="'$(dirname "$DIR")/charts/kubernetes-external-secrets' --name e2e"
   if [[ "$DISABLE_CUSTOM_RESOURCE_MANAGER" == "true" ]]; then
     HELM_TEMPLATE_EXTRA_ARGS="--set crds.create=true --set customResourceManagerDisabled=true"
     E2E_EXTRA_ARGS='--env="DISABLE_CUSTOM_RESOURCE_MANAGER=true"'
+  else
+    HELM_TEMPLATE_EXTRA_ARGS=''
+    E2E_EXTRA_ARGS=''
   fi
 fi
 
-helm template e2e "$(dirname "$DIR")/charts/kubernetes-external-secrets" \
+helm2 template "${HELM_TEMPLATE_ARGS}" \
   "${HELM_TEMPLATE_EXTRA_ARGS}" \
   --set image.repository=external-secrets \
   --set image.tag=test \
